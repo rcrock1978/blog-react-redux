b396b2fdef07ab3d82b9d6104c1c8be703c0d233
Raymund Caringal <rcrock1978@gmail.com>
2021-02-02 23:53:53 +0800

    blog copmplete

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React, { StrictMode } from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 
-import App from "./App";
+import App from "./components/App";
+import reducers from "./reducers";
+
+const store = createStore(reducers, applyMiddleware(thunk));
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>,
   rootElement
 );
